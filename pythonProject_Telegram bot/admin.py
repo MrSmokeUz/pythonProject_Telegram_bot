82f157817d4cdab132d1f3a698d8c00ef37a0765
EvilSamsa <ulugbekh789@gmail.com>
2024-12-30 23:28:40 +0500

    deleted telegram bot(He will posted in another rep)

--- pythonProject_Telegram bot/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Card
-
-@admin.register(Card)
-class CardAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at')
-    search_fields = ('title', 'description')
