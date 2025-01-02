a955c8f589827c79e8348aa9d90ce4f8d887e6bd
EvilSamsa <ulugbekh789@gmail.com>
2025-01-02 20:35:55 +0500

    Fixed bugs

--- adminCaesar/adminCaesar/settings.py
+++ adminCaesar/adminCaesar/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure)5g4gsm@%m&mp9lun=tbt2frw)=#^h!o!%x(_h=en%^-7$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [".herokuapp.com"]
+ALLOWED_HOSTS = [".herokuapp.com","127.0.0.1"]
 
 
 # Application definition
